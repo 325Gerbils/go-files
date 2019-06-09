beb867bc8cb817584d91cec4a592ea07dce45c93
Benjamin Cooley <benjamincooley81@gmail.com>
2019-06-09 16:49:50 -0700

    Update files.go

--- files.go
+++ files.go
@@ -3,6 +3,8 @@ package files
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 // Open returns the file contents as a string
@@ -17,6 +19,42 @@ func Write(data, filepath string) {
 	message := []byte(data)
 	err := ioutil.WriteFile(filepath, message, 0644)
 	check(err)
+}
+
+// List returns list of files in directory
+func List(dir string) []string {
+	files, err := ioutil.ReadDir(".")
+	check(err)
+	out := []string{}
+	for _, file := range files {
+		if !file.IsDir() {
+			out = append(out, file.Name())
+		}
+	}
+	return out
+}
+
+// ListAll Same as List() but scans subfolders too
+func ListAll(dir string) []string {
+	out := []string{}
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		out = append(out, path)
+		return nil
+	})
+	return out
+}
+
+// ListDir returns list of subdirectories in directory
+func ListDir(dir string) []string {
+	files, err := ioutil.ReadDir(".")
+	check(err)
+	out := []string{}
+	for _, file := range files {
+		if file.IsDir() {
+			out = append(out, file.Name())
+		}
+	}
+	return out
 }
 
 func check(err error) {
